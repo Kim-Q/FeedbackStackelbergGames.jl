702a72e335e3fc47b46f15899618bea142101691
Kim-Q <fumanqing@outlook.com>
2026-04-29 16:42:56 +0800

    clean version

--- python/feedback_stackelberg/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Python implementation of feedback Stackelberg game experiments."""
-
-from feedback_stackelberg.config import ExperimentConfig, PDIPConfig
-from feedback_stackelberg.experiments import ExperimentRunner
-
-__all__ = ["ExperimentConfig", "PDIPConfig", "ExperimentRunner"]
